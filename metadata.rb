d30cd00a0d1eff0af1a14f96f4347f43e6942b6b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-28 11:37:36 +0400

    Update ark dependency

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'gogs'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '2.1.1'
+version '2.1.2'
 description 'Installs and configures Gogs'
 long_description ::IO.read(::File.join(::File.dirname(__FILE__), 'README.md'))
 
@@ -10,7 +10,7 @@ recipe 'gogs', 'Installs and configures Gogs'
 
 depends 'instance', '~> 2.0.0'
 depends 'secret', '~> 1.0.0'
-depends 'ark', '~> 3.1.0'
+depends 'ark', '>= 3.1.0'
 depends 'supervisor', '~> 0.4.12'
 depends 'postgresql', '~> 6.1.1'
 depends 'database', '~> 6.1.1'
